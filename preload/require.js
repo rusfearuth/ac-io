5b18846b75d2dd6e04a3f8a5efffc9686db108ee
Vladlen <ultrashock98@gmail.com>
2016-02-24 17:26:28 +1000

    Fix setting and add module

--- preload/require.js
+++ preload/require.js
@@ -3,5 +3,8 @@
 /* Работа с "обещаниями" */
 exports.promise = require('bluebird');
 
+/* Работа с наследованием */
+exports.extend = require('merge-util');
+
 /* Работа с http */
 exports.request = require('request-promise');
